b9cf8483ad4894a289d0ca5c2771352e8216b246
01JAMIL <jamilbenbrahim1@gmail.com>
2023-05-03 12:50:28 +0100

    Update elastic search code

--- controllers/skill-search.controller.js
+++ controllers/skill-search.controller.js
@@ -56,8 +56,8 @@ const createDocument = async () => {
 const saveDataIntoDocument = asyncHandler(async (req, res) => {
 
     const { jobCode } = req.params
-    await createDocument()
     await client.indices.delete({ index: 'skills-by-job-code' });
+    await createDocument()
     const url = `https://test-web-service-3z32.onrender.com/api/skills/get-job-skill/${jobCode}`
     const response = await axios.get(url);
     const bulkBody = [];
